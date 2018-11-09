46f5a8ad7be2b470451305928fb5ed6ac87312b9
Shubhamoy Chakrabarty <me@shubhamoy.com>
2018-11-09 13:29:01 +0530

    Add first test case

--- test/SmartWallet_test.js
+++ test/SmartWallet_test.js
@@ -1,3 +1,25 @@
 let SmartWallet = artifacts.require("./SmartWallet.sol");
 
-contract('SmartWallet', (accounts) => { });
+contract('SmartWallet', (accounts) => {
+	let smartWalletInstance;
+	const owner = accounts[0];
+	const wallet1 = accounts[8];
+	const wallet2 = accounts[9];
+	const percentShare1 = 60;
+	const percentShare2 = 40;
+	const totalWallets = 2;
+
+	it("should be configurable", () => {
+		return SmartWallet.deployed()
+			.then((instance) => {
+				smartWalletInstance = instance;
+				return smartWalletInstance
+					.configureShare(totalWallets, 
+						[percentShare1, percentShare2], 
+						[wallet1, wallet2]);
+			})
+			.then((data) => {
+				assert.equal(data.receipt.status, 0x1, "Success");
+			})
+	});
+});
