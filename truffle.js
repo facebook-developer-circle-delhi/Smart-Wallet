ff842a381cffea63bb064aef8c534819354b8628
Shubhamoy Chakrabarty <me@shubhamoy.com>
2018-11-09 13:06:01 +0530

    Configure truffle to use ganache as eth instance

--- truffle.js
+++ truffle.js
@@ -15,4 +15,11 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    ganache: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*"
+    }
+  }
 };
